df9c7b56697907a1e74d28c4046d847a8e34fd9f
Chen Steenvoorden <csteenvo@student.codam.nl>
2022-03-06 15:46:25 +0100

    set normalized coordinates in actions

--- src/actions.c
+++ src/actions.c
@@ -1,5 +1,18 @@
 #include "cluster.h"
 
+static void swap(void *a_void, void *b_void, size_t size)
+{
+	unsigned char *a_char = a_void;
+	unsigned char *b_char = b_void;
+
+	for (size_t i = 0; i < size; i++)
+	{
+		a_char[i] ^= b_char[i];
+		b_char[i] ^= a_char[i];
+		a_char[i] ^= b_char[i];
+	}
+}
+
 cell_t *game_update(game_t *game, cell_t *cell)
 {
 	cell_t *next = cell->neighbors[game->gravity];
@@ -11,9 +24,9 @@ cell_t *game_update(game_t *game, cell_t *cell)
 	cell_t *dropped = game_update(game, next);
 	if (cell->value == 0)
 		return dropped;
-	int tmp = cell->value;
-	cell->value = dropped->value;
-	dropped->value = tmp;
+	swap(&cell->value, &dropped->value, sizeof(cell->value));
+	swap(&cell->old_x, &dropped->old_x, sizeof(cell->old_x));
+	swap(&cell->old_y, &dropped->old_y, sizeof(cell->old_y));
 	return cell->neighbors[(game->gravity + 3) % 6];
 }
 
@@ -28,5 +41,6 @@ void game_drop(game_t *game, int q, int r, int s, int value)
 {
 	cell_t *cell = game_get(game, q, r, s);
 	cell->value = value;
+	coord_convert(&cell->old_x, &cell->old_y, q, r, s);
 	game_update(game, cell);
 }
