c445d753681cc8c7945f4fcda57edd9b18247ec4
Abel Van bergen en henegouwen <avan-ber@f0r5s11.codam.nl>
2022-03-06 14:03:12 +0100

    onzin

--- include/cluster.h
+++ include/cluster.h
@@ -3,13 +3,23 @@
 
 #include <stddef.h>
 
-struct cell {
-	struct cell *neighbors[6];
-};
+/* -=-=- Define -=-=- */
 
-struct game {
-	struct cell *cells;
-	size_t cell_count;
-};
+//colors
+# define COLOR_RED 0xFF000088
+# define COLOR_GREEN 0x00FF0088
+
+//window
+# define WINDOW_WIDTH 800
+# define WINDOW_HEIGHT 600
+
+typedef struct s_cell {
+	struct cell *neighbors[6];
+}				t_cell;
+
+struct s_game {
+	struct cell*	cells;
+	size_t			cell_count;
+}					t_game;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,12 +1,23 @@
+
+#include "cluster.h"
 #include "MLX42/MLX42.h"
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(void)
 {
-	mlx_t *mlx;
+	mlx_t		*mlx;
+	mlx_image_t	*image;
 
-	mlx = mlx_init(800, 600, "cluster", 1);
-	puts("Hello, World!");
+	mlx = mlx_init(WINDOW_WIDTH, WINDOW_HEIGHT, "cluster", 1);
+	image = mlx_new_image(mlx, WINDOW_WIDTH, WINDOW_HEIGHT);
+	for (int y = 0; y < 100; y++)
+		for (int x = 0; x < 100; x++)
+			mlx_put_pixel(image, x, y, COLOR_RED);
+	for (int y = 300; y < 400; y++)
+		for (int x = 400; x < 500; x++)
+			mlx_put_pixel(image, x, y, COLOR_GREEN);
+	mlx_image_to_window(mlx, image, 0, 0);
+	mlx_loop(mlx);
 	return (EXIT_SUCCESS);
 }
