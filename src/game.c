5cbba52118d898ec3d6a6f557c6b36cf21e3d155
Chen Steenvoorden <csteenvo@student.codam.nl>
2022-03-08 10:12:01 +0100

    bot interaction stuff

--- src/game.c
+++ src/game.c
@@ -1,15 +1,9 @@
 #include "cluster.h"
 #include <stdio.h>
 #include <string.h>
-
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <errno.h>
 
-int init_player(const char* path, player_t* player)
+static int popen2(const char* path, player_t* player)
 {
 	int in[2], out[2];
 	pipe(in);
@@ -41,36 +35,72 @@ void game_start(game_t *game, player_t *players)
 	fprintf(players[1].out, "init %d %d %d 1\n", game->color_count, game->cell_count / game->color_count, game->grid_size);
 }
 
-void game_turn(game_t *game, player_t *players)
+int game_take_random(game_t *game)
 {
-	int a = 2, b = 3;
+	int total = 0;
+	int value = game->turn * game->color_count / 2;;
+	for (int i = 0; i < game->color_count / 2; i += 1) {
+		total += game->chip_counts[i + value];
+	}
+	if (total == 0)
+		return -1;
+	int index = rand() % total;
+	while (index >= game->chip_counts[value]) {
+		index -= game->chip_counts[value];
+		value += 1;
+	}
+	return value;
+}
+
+int game_turn(game_t *game, player_t *players)
+{
+	int a, b;
 	int q, r, s, value;
 	char action[256];
+	a = game_take_random(game);
+	b = game_take_random(game);
+	if (a == -1 || b == -1)
+		return !game->turn;
 	fprintf(players[game->turn].out, "chips %d %d\n", a, b);
 	fscanf(players[game->turn].in, "%255s", action);
 	if (strcmp(action, "rotate") == 0) {
 		fscanf(players[game->turn].in, "%d", &value);
+		if (value < 0 || value >= 6)
+			return !game->turn;
 		fprintf(players[!game->turn].out, "rotate %d\n", value);
 		game_rotate(game, value);
 	} else if (strcmp(action, "drop") == 1) {
 		fscanf(players[game->turn].in, "%d %d %d %d", &q, &r, &s, &value);
+		cell_t *cell = game_get(game, q, r, s);
+		if (cell == NULL || cell->value != -1)
+			return !game->turn;
+		if (value < game->turn * game->color_count / 2)
+			return !game->turn;
+		if (value >= (game->turn + 1) * game->color_count / 2)
+			return !game->turn;
 		fprintf(players[!game->turn].out, "drop %d %d %d %d\n", q, r, s, value);
 		game_drop(game, q, r, s, value);
-	}
+	} else
+		return !game->turn;
+	int winner = game_winner(game);
+	if (winner != -1)
+		return winner * 2 / game->color_count;
+	return -1;
 }
 
 void game_loop(const char *path1, const char *path2)
 {
 	game_t game;
 	player_t players[2];
+	int winner = -1;
 
 	game_init(NULL, &game, 4, 1);
-	init_player(path1, &players[0]);
-	init_player(path2, &players[1]);
+	popen2(path1, &players[0]);
+	popen2(path2, &players[1]);
 	game_start(&game, players);
 
-	while (1) {
-		game_turn(&game, players);
+	while (winner != -1) {
+		winner = game_turn(&game, players);
 		game.turn = !game.turn;
 	}
 }
