02068ea92c0bb6f50deed53284a903ea18872478
Chen Steenvoorden <csteenvo@student.codam.nl>
2022-03-13 15:21:36 +0100

    better bot

--- src/bots.c
+++ src/bots.c
@@ -76,8 +76,11 @@ static void compute_pos(int pos, int size, int gravity, int *q, int *r, int *s)
 	}
 }
 
+static bool timed_out;
+
 static void handler(int sig)
 {
+	timed_out = true;
 	(void) sig;
 }
 
@@ -283,23 +286,23 @@ int game_postturn_drop(game_t *game, int q, int r, int s, int pos, int value)
 
 static int handle_fetch(game_t *game)
 {
-	char target[8];
+	char target[16];
 
-	if (fscanf(game->players[game->turn].in, "%7s", target) != 1) {
+	if (fscanf(game->players[game->turn].in, "%15s", target) != 1) {
 		if (game->config->debug)
 			fprintf(stderr, "Player %d did not give fetch target\n", game->turn + 1);
 		return 0;
 	}
 
-	if (strcmp(target, "cells")) {
+	if (strcmp(target, "cells") == 0) {
 		fprintf(game->players[game->turn].out, "cell_count %d\n", game->cell_count);
 		for (int i = 0; i < game->cell_count; i++) {
 			cell_t *cell = &game->cells[i];
 			fprintf(game->players[game->turn].out, "cell %d %d %d %d\n", cell->q, cell->r, cell->s, cell->chip.value);
 		}
-	} else if (strcmp(target, "gravity")) {
+	} else if (strcmp(target, "gravity") == 0) {
 		fprintf(game->players[game->turn].out, "gravity %d\n", game->gravity);
-	} else if (strcmp(target, "walls")) {
+	} else if (strcmp(target, "walls") == 0) {
 		int wall_count = 0;
 		for (int i = 0; i < game->cell_count; i++)
 			if (game->cells[i].wall)
@@ -324,7 +327,13 @@ int game_turn(game_t *game)
 	fprintf(game->players[game->turn].out, "chips %d %d\n", game->chip_a, game->chip_b);
 
 	// Get action from player
+	timed_out = false;
 start:
+	if (timed_out) {
+		if (game->config->debug)
+			fprintf(stderr, "Player %d timed out\n", game->turn + 1);
+		return check_winner(!game->turn);
+	}
 	if (fscanf(game->players[game->turn].in, "%7s", action) != 1) {
 		disarm_timer();
 		if (game->config->debug)
--- test.c
+++ test.c
@@ -2,10 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <sys/time.h>
 #include <unistd.h>
 
-void read_action(char *action, int *pos, int *value)
+static unsigned long mix(unsigned long a, unsigned long b, unsigned long c)
+{
+	a = (a - b - c) ^ (c >> 13);
+	b = (b - c - a) ^ (a << 8);
+	c = (c - a - b) ^ (b >> 13);
+	a = (a - b - c) ^ (c >> 12);
+	b = (b - c - a) ^ (a << 16);
+	c = (c - a - b) ^ (b >> 5);
+	a = (a - b - c) ^ (c >> 3);
+	b = (b - c - a) ^ (a << 10);
+	c = (c - a - b) ^ (b >> 15);
+	return c;
+}
+
+static int next(int max)
+{
+	return (rand() / 256) % max;
+}
+
+static void read_action(char *action, int *pos, int *value)
 {
 	scanf("%255s", action);
 	if (strcmp(action, "rotate") == 0) {
@@ -13,6 +31,49 @@ void read_action(char *action, int *pos, int *value)
 	} else if (strcmp(action, "drop") == 0) {
 		scanf("%d %d", pos, value);
 	}
+}
+
+static int read_gravity(void)
+{
+	int gravity;
+	printf("fetch gravity\n");
+	scanf(" gravity %d", &gravity);
+	return gravity;
+}
+
+static void compute_pos(int pos, int size, int gravity, int *q, int *r, int *s)
+{
+	size -= 1;
+	*q = pos;
+	if (pos < 0) {
+		*r = -size - pos;
+		*s = size;
+	} else {
+		*r = -size;
+		*s = size - pos;
+	}
+	while (gravity > -3)
+	{
+		int tmp = *q;
+		*q = -*r;
+		*r = -*s;
+		*s = -tmp;
+		gravity -= 1;
+	}
+}
+
+static int read_emptiness(int q, int r, int s)
+{
+	int q2, r2, s2, value;
+	int count, result = -1;
+	printf("fetch cells\n");
+	scanf(" cell_count %d", &count);
+	for (int i = 0; i < count; i++) {
+		scanf(" cell %d %d %d %d", &q2, &r2, &s2, &value);
+		if (q == q2 && r == r2 && s == s2)
+			result = (value == -1);
+	}
+	return result;
 }
 
 int main(void)
@@ -23,21 +84,32 @@ int main(void)
 	float timeout;
 	int a, b;
 	int c;
+	int q, r, s;
 	setbuf(stdout, NULL);
 	setbuf(stdin, NULL);
 	setbuf(stderr, NULL);
 	scanf(" init %d %d %d %f %d", &color_count, &chip_count, &grid_size, &timeout, &id);
-	c = arc4random() % 6;
-	printf("color %d\n", c == 2 ? 0 : c);
+	srand(mix(clock(), time(NULL), getpid()));
+	c = next(5);
+	printf("color %d\n", c >= 2 ? c + 1 : c);
 	if (id == 1) {
 		read_action(action, &pos, &value);
 	}
 	while (1) {
 		scanf(" chips %d %d", &a, &b);
-		if (arc4random() % 2 == 0)
-			printf("rotate %d\n", arc4random() % 6);
-		else
-			printf("drop %d %d\n", (int) (arc4random() % (grid_size * 2 - 1)) - grid_size + 1, a);
+		if (next(2) == 0 && b != -1) {
+			value = next(5);
+			printf("rotate %d\n", value >= read_gravity() ? value + 1 : value);
+		} else {
+			while (1) {
+				pos = next(grid_size * 2 - 1) - grid_size + 1;
+				compute_pos(pos, grid_size, read_gravity(), &q, &r, &s);
+				if (read_emptiness(q, r, s)) {
+					printf("drop %d %d\n", pos, a);
+					break;
+				}
+			}
+		}
 		read_action(action, &pos, &value);
 	}
 	return EXIT_SUCCESS;
